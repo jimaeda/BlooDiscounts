a2ef94816b9af2fd7913981d3d808ceaf1a0ec49
Jim Aeda <reki.imaeda@gmail.com>
2019-08-17 21:27:51 -0300

    test

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module BlooDiscounts
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.assets.enabled = false
+    config.secret_key_base = 'blipblapblup'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
