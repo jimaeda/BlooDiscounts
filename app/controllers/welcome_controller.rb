543aaa96f11cc3efe6be07df429c167d32fdf3b3
Jim Aeda <reki.imaeda@gmail.com>
2019-08-31 16:38:20 -0300

    Learning some things

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,11 @@
 class WelcomeController < ApplicationController
+	def index
+		if (params.has_key?(:id) && params.has_key?(:user))
+			@id = params['id']
+			@user = params[:user]
+		end
+	end
+	def sample
+		@controller_message = "Hello from controller"
+	end
 end
