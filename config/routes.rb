5c2b6d215b94039226ece4c02d80180abd3d8877
Jim Aeda <reki.imaeda@gmail.com>
2019-09-06 22:00:09 -0300

    hello world

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get 'welcome/index'
-  match ':controller(/:action(/:id))', :via => :get
-  root 'welcome#index'
+  # match ':controller(/:action(/:id))', :via => :get
+  # root 'welcome#index'
+  root 'application#hello'
 end
