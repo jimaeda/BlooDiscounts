853e8ba35a670069644558e46e5f6720ed3e6c41
Jim Aeda <reki.imaeda@gmail.com>
2019-09-23 22:13:08 -0300

    devise implanted

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'login' => 'sessions#new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
-
-  resources :users, only: [:new, :create, :show]
+  root to: 'pages#home'
+  devise_for :users
 end
