f1c2e6d010f3cc7b3f8fb20f7809c014d99b1469
leovamos <leonardo.victorms@usp.br>
2019-09-25 09:54:59 -0300

    Add parameters in sign up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_25_013400) do
+ActiveRecord::Schema.define(version: 2019_09_25_103824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2019_09_25_013400) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
     t.boolean "admin"
+    t.integer "bloodtype"
+    t.date "birthdate"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
