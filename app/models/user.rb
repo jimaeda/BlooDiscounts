b73f115e7cc0711464e860da8e382dc86ec32ad7
Jim Aeda <reki.imaeda@gmail.com>
2019-09-28 08:51:06 -0300

    removed files

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  enum bloodtype: [:'A-',:'A+',:'B-',:'B+',:'AB-',:'AB+',:'O-',:'O+']
-end
--- config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-	root to: 'pages#home'
-	devise_for :users
-end
