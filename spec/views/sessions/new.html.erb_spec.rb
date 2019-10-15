944df89d952ceb6169016e01e6a55b31cb2a8896
Jim Aeda <reki.imaeda@gmail.com>
2019-10-15 11:18:59 -0300

    added

--- spec/views/sessions/new.html.erb_spec.rb
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
+  
+describe "Successfull Login" do 
+  it "Sucessful administrator login" do
+    Admin.create(adm_username: "alexandrefreire", password: "123456", adm_name: "Alexandre Freire", adm_cpf: "123456", hospital_name: "Hospital")
+    visit login_path
+    fill_in "Adm username", with: 'alexandrefreire'
+    fill_in "Password", with: '123456'
+    find('input[name="commit"]').click
+    expect(page).to have_content('Alexandre Freire')
+  end
+end
 
-RSpec.describe "sessions/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "Unsuccessfull Login" do 
+  it "Sucessful administrator login" do
+    Admin.create(adm_username: "alexandrefreire", password: "123456", adm_name: "Alexandre Freire", adm_cpf: "123456", hospital_name: "Hospital")
+    visit login_path
+    fill_in 'Adm username', with: 'alexandrefreire'
+    fill_in 'Password', with: '111111'
+    find('input[name="commit"]').click
+    expect(page).to have_no_content('Alexandre Freire')
+  end
 end
