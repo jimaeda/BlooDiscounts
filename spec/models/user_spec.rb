64521dd075dbbeefa8057fe0362750e0aa9c357f
Denise <denisekfa@gmail.com>
2019-10-18 19:33:27 -0300

    Remoção de partes inuteis

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}" idk
+
+  context "Testando view do usuário" do
+    it "Cadastrar um usuario" do        # ação
+      visit '/users/sign_up'            # caminho para cadastro do user
+
+      within("#new_user") do #nome do form
+        fill_in 'user_email', :with => 'user@example.com' # preenche cx de texto
+        select 'O-', from: 'user_bloodtype' # preenche uma cx de seleção
+        fill_in 'user_birthdate', :with => '1999/01/01'
+        fill_in 'user_password', :with => '123456'
+        fill_in 'user_password_confirmation', :with => '123456'
+      end
+
+      click_button 'commit' # clica no botão de Cadastrar
+      expect(page).to have_content 'Welcome! You have signed up successfully.'
+    end
+  end
+
 end
