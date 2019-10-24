3ee0b3e83aa923f8a5953f66dff1296074465439
leovamos <38430921+leovamos@users.noreply.github.com>
2019-10-23 21:50:44 -0300

    Adição de informação do usuário e ajustes (#20)
    * Adição de informação do usuário, ajustes nas telas e na alteração de informação do usuário

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,10 @@ class ApplicationController < ActionController::Base
 
 	protected
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.permit(:sign_up, keys: [:bloodtype,:birthdate])
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name,:last_name,
+			:weight,:age,:height,:city,:state,:country,:points,:bloodtype,:birthdate])
+		devise_parameter_sanitizer.permit(:account_update, keys: [:first_name,:last_name,
+			:weight,:age,:height,:city,:state,:country,:points,:bloodtype,:birthdate])
 	end
 
 	def after_sign_in_path_for(resource)
