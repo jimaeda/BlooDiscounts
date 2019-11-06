64814b994e9f6dbfb543a1c8e8705f3a6752b146
Denise <denisekfa@gmail.com>
2019-11-06 10:07:34 -0300

    Config do Geocoder

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,6 +10,7 @@ class PlacesController < ApplicationController
   # GET /places/1
   # GET /places/1.json
   def show
+    @place = Place.find(params[:id])
   end
 
   # GET /places/new
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,11 @@
 class Place < ApplicationRecord
+  =begin
+  def full_address
+    [pais, cidade, rua].compact.join(‘,’)
+  end
+  geocoded_by :full_address
+  after_validation :geocode, if: ->(obj){
+    obj.address.present? and obj.address_changed?
+  }
+  =end
 end
