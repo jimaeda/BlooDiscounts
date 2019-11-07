31e0ac18aae36a673e8f2bfb5ac31f17465f3005
Jim Aeda <reki.imaeda@gmail.com>
2019-11-06 21:49:15 -0300

    test passed

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -14,7 +14,47 @@ Dado("a página de cadastro do BlooDiscounts") do
     visit 'http://localhost:3000/users/sign_up'
 end
 
+Dado("que eu acesso o cadastro de recompensas") do
+    visit 'http://localhost:3000/rewards/new'
+end
+
 # Quando/When
+
+# Nome faltando 
+Quando("eu digitar nada, {string}, {int} e {int}") do |categoria, quantidade, custo|
+    find('input[id=reward_category]').set categoria
+    find('input[id=reward_quantity]').set quantidade
+    find('input[id=reward_cost]').set custo
+end
+  
+# Categoria faltando 
+Quando("eu digitar {string}, nada, {int} e {int}") do |nome, quantidade, custo|
+    find('input[id=reward_name]').set nome
+    find('input[id=reward_quantity]').set quantidade
+    find('input[id=reward_cost]').set custo
+end
+
+# Quantidade faltando
+Quando("eu digitar {string}, {string}, nada e {int}") do |nome, categoria, custo|
+    find('input[id=reward_category]').set categoria
+    find('input[id=reward_name]').set nome
+    find('input[id=reward_cost]').set custo
+end
+  
+# Valor faltando
+Quando("eu digitar {string}, {string}, {int} e nada") do |nome, categoria, quantidade|
+    find('input[id=reward_category]').set categoria
+    find('input[id=reward_name]').set nome
+    find('input[id=reward_quantity]').set quantidade
+end
+  
+# Caso perfeito
+Quando("eu digitar {string}, {string}, {int} e {int}") do |nome, categoria, quantidade, custo|
+    find('input[id=reward_category]').set categoria
+    find('input[id=reward_name]').set nome
+    find('input[id=reward_quantity]').set quantidade
+    find('input[id=reward_cost]').set custo  
+end
 
 Quando('eu for para tela de edição de perfil') do
     find('input[value="Exibir perfil"]').click
@@ -89,6 +129,21 @@ end
   
 # Então/Then
 
+# Clicar no botão cadastrar
+Então("devo clicar em create reward") do
+    click_button "Create Reward"
+end
+
+# Cadastro com sucesso
+Então("devo ir para a tela com os dados cadastrados e a mensagem {string}") do |string|
+    expect(page).to have_content 'Recompensa cadastrada com sucesso.'
+end
+
+# Não houve cadastro
+Então("devo continuar na mesma tela") do
+    expect(page).to have_content 'Cadastro de Recompensa'    
+end
+
 Então('eu devo ir para a página de admin') do
     expect(page).to have_no_field('Admin Username')
 end
@@ -113,4 +168,3 @@ end
 Então("devo ter a seguinte mensagem {string}") do |mensagem|
     expect(page).to have_content mensagem
 end
-  
