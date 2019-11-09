a086671e84df6cdd587fe92b7b5a242388993cd6
Gabriel Viana <gabrielviana57@gmail.com>
2019-11-09 10:20:23 -0300

    Fix do jQuery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,12 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require("bootstrap")
-require("jquery")
+//Import Bootstrap and Dependencies
+require('bootstrap')
+//Set $ with jQuery
+global.$ = require("jquery")
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
