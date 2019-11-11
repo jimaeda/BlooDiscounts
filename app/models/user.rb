742830e7861ccc0f119f1507f8ace34d655714ff
Gabriel Viana <gabrielviana57@gmail.com>
2019-11-10 21:14:06 -0300

    Alteração da Validação do User

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
   enum bloodtype: [:'A-',:'A+',:'B-',:'B+',:'AB-',:'AB+',:'O-',:'O+']
+
   validates :email, uniqueness: true
+  validates :weight, :numericality => {greater_than: 0}, presence: true
+  validates :height, :numericality => {greater_than: 0}, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :country, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :birthdate, presence: true
 
   def set_age
     self.age = (Time.now.year - self.birthdate.year) + (Time.now.month >
